16d6d8d8c6b45c7c0cde1823544f9d60990001ef
이하늘 <hee1149kr@naver.com>
2020-01-07 19:41:03 +0900

    Upload All file

---
+++ flutter_test_app/lib/src/random_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomList extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _RandomListState();
+  }
+}
+
+class _RandomListState extends State<RandomList>{
+  @override
+  Widget build(BuildContext context) {
+    final randomWord = WordPair.random();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("test app"),
+      ),
+      body: Center(child : Text(randomWord.asPascalCase, textScaleFactor: 1.5,)),
+    );
+  }
+}
